31d9b4b56d1fe6760cc0061399521bf054f4e0f0
Amos Tan <alphatrl1997@gmail.com>
2024-01-29 23:31:28 +0800

    WIP(android): Setting props

--- android/src/main/java/com/rnemojipicker/RNEmojiPickerViewManager.kt
+++ android/src/main/java/com/rnemojipicker/RNEmojiPickerViewManager.kt
@@ -1,13 +1,13 @@
 package com.rnemojipicker
 
+import android.util.Log
 import androidx.emoji2.emojipicker.EmojiPickerView
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 
 @ReactModule(name = RNEmojiPickerViewManager.NAME)
-class RNEmojiPickerViewManager :
-  EmojiPickerViewManagerSpec<EmojiPickerView>() {
+class RNEmojiPickerViewManager : EmojiPickerViewManagerSpec<EmojiPickerView>() {
 
   override fun getName(): String {
     return NAME
@@ -17,14 +17,17 @@ class RNEmojiPickerViewManager :
     return EmojiPickerView(context)
   }
 
-  @ReactProp(name = "emojiColumns")
-  override fun setEmojiGridColumns(view: EmojiPickerView?, emojiGridColumns: Int) {
-    view?.emojiGridColumns = emojiGridColumns
+  @ReactProp(name = "emojiColumns", defaultInt = 0)
+  override fun setEmojiGridColumns(view: EmojiPickerView, emojiGridColumns: Int) {
+    Log.d("emojiColumns", emojiGridColumns.toString())
+    view.emojiGridColumns = emojiGridColumns
   }
 
-  @ReactProp(name = "emojiRows")
-  override fun setEmojiGridRows(view: EmojiPickerView?, emojiGridRows: Float) {
-    view?.emojiGridRows = emojiGridRows
+
+  @ReactProp(name = "emojiRows", defaultFloat = 0f)
+  override fun setEmojiGridRows(view: EmojiPickerView, emojiGridRows: Float) {
+    Log.d("emojiRows", emojiGridRows.toString())
+    view.emojiGridRows = emojiGridRows
   }
 
   companion object {
--- android/src/oldarch/EmojiPickerViewManagerSpec.kt
+++ android/src/oldarch/EmojiPickerViewManagerSpec.kt
@@ -5,8 +5,8 @@ import androidx.emoji2.emojipicker.EmojiPickerView
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.SimpleViewManager
 
-abstract class EmojiPickerViewManagerSpec<T : EmojiPickerView> : SimpleViewManager<T>() {
-  abstract fun setEmojiGridColumns(view: T?, emojiGridColumns: Int)
+abstract class EmojiPickerViewManagerSpec<T : View> : SimpleViewManager<T>() {
+  abstract fun setEmojiGridColumns(view: T, emojiGridColumns: Int)
 
-  abstract fun setEmojiGridRows(view: T?, emojiGridRows: Float)
+  abstract fun setEmojiGridRows(view: T, emojiGridRows: Float)
 }
